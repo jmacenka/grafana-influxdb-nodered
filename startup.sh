3b16aadbfa46a1c5ba038196043e225e7ad9befa
Jan Macenka <jan.macenka@gmail.com>
2019-08-28 22:02:46 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 sudo docker-compose up -d
 curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE mydb'
 
-echo "Done"
+echo "All Done, Container is up and running. Visite http://$(hostname -I | cut -d' ' -f1):1880 or http://$(hostname -I | cut -d' ' -f1):3000"
