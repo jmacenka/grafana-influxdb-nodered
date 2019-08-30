19bd8ef671c32ded1a42150c3f7b1af399f027eb
Jan Macenka <jan.macenka@gmail.com>
2019-08-30 11:14:11 +0000

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,13 @@
 # Remove everything and release ocupied space
 
-yn = read -p "Delete all running Containers? y/n"
+read -p "Delete all running Containers? y/n" yn
 if [ $yn == 'y' ]
 then
     sudo docker rm -f $(sudo docker ps -aq)
 fi
 sudo docker volume prune
 sudo docker network prune
-yn = read -p "Remove all container images? y/n"
+read -p "Remove all container images? y/n" yn
 if [ $yn == 'y' ]
 then
     sudo docker rmi -f $(sudo docker images -aq)
