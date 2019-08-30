1fbee6f37eb1feb63dc4d041e1089d84634cf2ee
Jan Macenka <jan.macenka@gmail.com>
2019-08-30 11:12:37 +0000

    Fixed typo

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,15 @@
 # Remove everything and release ocupied space
 
-read -p "Delete all running Containers? y/n\n"
-if [ $yn == 'y' ]then
+read -p "Delete all running Containers? y/n"
+if [ $yn == 'y' ]
+then
     sudo docker rm -f $(sudo docker ps -aq)
 fi
 sudo docker volume prune
 sudo docker network prune
-read -p "Remove all container images? y/n\n"
-if [ $yn == 'y' ]then
+read -p "Remove all container images? y/n"
+if [ $yn == 'y' ]
+then
     sudo docker rmi -f $(sudo docker images -aq)
 fi
 echo ""
