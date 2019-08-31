b0d99c1fa601a0b88c3d845b0f0ada13b1fd76a1
Jan Macenka <jan.macenka@gmail.com>
2019-08-31 23:47:29 +0200

    Minor cleanup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,8 @@
-sudo docker-compose up -d
-curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE mydb'
+echo "Starting the entire project..."
 export $(cat .env | xargs)
+sudo docker-compose up -d
+curl -XPOST 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE $INFLUX_DEFAULT_DB"
+if [ $NODE_RED_SLIM == "true" ];then
+  echo "Using node-red:slim => Won't auto-install influxdb-package in node red but is 800 MB smaler!"
+fi
 echo "All Done, Container is up and running. Visite http://$(hostname -I | cut -d' ' -f1):$NODE_RED_PORT (for backend) or http://$(hostname -I | cut -d' ' -f1):$GRAFANA_PORT (for frontend)"
