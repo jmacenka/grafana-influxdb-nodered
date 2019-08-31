e9e4ccf3156b58033c0a7fbef7a331b09a848752
Jan Macenka <jan.macenka@gmail.com>
2019-08-31 23:51:05 +0200

    Added message on name of newly generated db in startup-script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 echo "Starting the entire project..."
 export $(cat .env | xargs)
 sudo docker-compose up -d
+echo "Creating new database: $INFLUX_DEFAULT_DB"
 curl -XPOST 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE $INFLUX_DEFAULT_DB"
 if [ $NODE_RED_SLIM == "true" ];then
   echo "Using node-red:slim => Won't auto-install influxdb-package in node red but is 800 MB smaler!"
