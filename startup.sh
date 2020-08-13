8d86b7aba836496e9cb5ded512686c4747ad8c02
Jan Macenka <jan.macenka@gmail.com>
2020-08-13 22:43:20 +0200

    Added portainer support

--- startup.sh
+++ startup.sh
@@ -6,4 +6,7 @@ curl -XPOST 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE $I
 if [ $NODE_RED_SLIM == "true" ];then
   echo "Using node-red:slim => Won't auto-install influxdb-package in node red but is 800 MB smaler!"
 fi
-echo "All Done, Container is up and running. Visite http://$(hostname -I | cut -d' ' -f1):$NODE_RED_PORT (for backend) or http://$(hostname -I | cut -d' ' -f1):$GRAFANA_PORT (for frontend)"
+echo "All Done, Container is up and running."
+echo "Visite http://$(hostname -I | cut -d' ' -f1):$NODE_RED_PORT (for backend)."
+echo "Visite http://$(hostname -I | cut -d' ' -f1):$GRAFANA_PORT (for frontend)"
+echo "Visite http://$(hostname -I | cut -d' ' -f1):$PORTAINER_PORT (for container management)"
